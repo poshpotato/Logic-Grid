54c668aac8c36803fe679fbf760eb966eeecbdd7
poshpotato <56280387+poshpotato@users.noreply.github.com>
2022-04-17 14:31:10 +1200

    Updated tests on page

--- src/test.ts
+++ src/test.ts
@@ -8,5 +8,19 @@ function fiveSetsOfFiveTest(headChef: Node){
         }
         testSetArray.push(testSet);
     }
-    appendSetInteractionGrid(headChef, testSetArray);
+    appendSetLogicTable(headChef, testSetArray);
+}
+
+function simpleSizedSetsTest(headChef: Node, sets: number, items: number){
+    var testSetArray:Array<Array<String>> = new Array<Array<String>>();
+    for(var i: number = 0; i < sets;i++){
+        //i is for sets
+        var testSet:Array<String> = new Array<String>();
+        for(var j: number = 0; j < items; j++){
+            testSet.push("Set " + i + ", item" + j);
+        }
+        testSetArray.push(testSet);
+    }
+    appendSetLogicTable(headChef, testSetArray);
+
 }
